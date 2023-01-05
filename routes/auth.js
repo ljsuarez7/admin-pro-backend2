12da8fd46b06f248ec250d2e08c484a8bb01a205
ljsuarez7@gmail.com <ljsuarez7@gmail.com>
2023-01-05 10:11:57 +0100

    seccion 12

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const {Router} = require('express');
 const { check } = require('express-validator');
-const {login} = require('../controllers/auth');
+const {login, googleSignIn} = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -10,5 +10,10 @@ router.post('/', [
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/google', [
+    check('token', 'El token de google es obligatorio').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 module.exports = router;
