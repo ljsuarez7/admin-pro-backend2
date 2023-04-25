9d0b04c48aecc0998e4683a4114eeccff091e8cd
ljsuarez7@gmail.com <ljsuarez7@gmail.com>
2023-04-25 18:29:15 +0200

    SPA CONTROL

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -27,6 +28,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 app.use('/api/login', require('./routes/auth'));
+
+//Dar controlar a Angular de las rutas
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+});
 
 
 app.listen(process.env.PORT, () => {
